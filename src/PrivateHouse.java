137b305103244b719536efdb76265bd265b67203
Olexander Zhurba <starkyzh@gmail.com>
2023-05-11 15:45:24 +0200

    add class PrivateHouse

---
+++ src/PrivateHouse.java
@@ -0,0 +1,16 @@
+public class PrivateHouse extends House {
+
+  /*
+  Создайте классы "Частный дом" и "Многоквартирный дом" - наследники класса "Дом".
+  В методе toString() выводите в удобном виде всю известную информацию о доме.
+
+   */
+  public PrivateHouse(String address) {
+    super(address);
+  }
+
+  @Override
+  public String toString() {
+    return "This is private house with address: " + getAddress();
+  }
+}
