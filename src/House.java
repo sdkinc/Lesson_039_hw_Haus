c8a94ead420c6be6ffdc04a7f40c3b4c48848b6b
Olexander Zhurba <starkyzh@gmail.com>
2023-05-11 15:43:32 +0200

    add abstract class House

---
+++ src/House.java
@@ -0,0 +1,19 @@
+public abstract class House {
+  /*
+      В качестве аргумента конструктора принимайте адрес.
+      Напишите для адреса сеттер и геттер.
+   */
+  private String address;
+
+  public House(String address){
+    this.address = address;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+}
